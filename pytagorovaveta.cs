b20740db73f3bb1ef0a562d0001f363562da8cc0
Zeese421 <113968624+Zeese421@users.noreply.github.com>
2022-10-11 08:58:48 +0200

    Add files via upload

---
+++ pytagorovaveta.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+            Console.WriteLine("Napiš délku strany A");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Napiš délku strany B");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Napiš délku strany C");
+            c = double.Parse(Console.ReadLine());
+
+            if (a > c)
+            {
+                double tmp = c;
+                c = a;
+                a = tmp;
+            }
+            if (b > c)
+            {
+                double tmp = c;
+                c = b;
+                b = tmp;
+            }
+
+
+            if (c*c == (a * a + b * b))
+            {
+                Console.WriteLine("Trojúhelník je pravoúhlý");
+            }
+
+            else if (c*c != (a * a + b * b))
+
+            {
+                Console.WriteLine("Trojúhelník není pravoúhlý");
+            }
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
